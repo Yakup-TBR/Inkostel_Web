9e381daafd990b2aa589b8346580eee685ee6b6b
bintangrayhan <bintangrayhan7@gmail.com>
2024-04-21 18:26:09 +0700

    menambahkan command detailkos
    menambahkan command di detailkos terkait penggunaan js

--- INKOSTEL/resources/views/detailKos.blade.php
+++ INKOSTEL/resources/views/detailKos.blade.php
@@ -117,7 +117,7 @@
                 <h6>Alamat : {{ $carikos->alamat }}</h6>
             </div>
 
-
+            <!-- ini adalah bagian js untuk  mengatur format harga dari data base -->
             <script>
                 function formatRupiah(angka) {
                     var formatter = new Intl.NumberFormat('id-ID', {
